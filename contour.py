385b1464c219cb9e9805265bcbb45d82d341272e
Marcos di Silva <mdsmus@gmail.com>
2010-04-12 10:59:17 -0300

    Inserts functions and docstrings

--- contour.py
+++ contour.py
@@ -5,6 +5,15 @@ from __future__ import print_function
 import subprocess
 
 
+def merge(seq):
+    '''Merge Sequences.'''
+    merged = []
+    for s in seq:
+        for x in s:
+            merged.append(x)
+    return merged
+
+
 def contour_class(contour_list):
     '''Returns the contour class of a given contour.'''
     sorted_contour = sorted(list(set(contour_list)))
@@ -21,6 +30,15 @@ def contour_subsets(contour, n):
     given contour.'''
     return [contour_class(contour[i:n + i])
             for i in range((len(contour) - (n - 1)))]
+
+
+def item_count(data):
+    '''Counts items in a list of lists'''
+    sorted_subsets = sorted(data)
+    tuples = [tuple(x) for x in sorted_subsets]
+    contour_type = list(set(tuples))
+    counted_contours = [[x, tuples.count(x)] for x in contour_type]
+    return sorted(counted_contours, key=lambda x: x[1], reverse=True)
 
 
 def contours_count(contour, n):
@@ -57,10 +75,35 @@ def freq_process(filename):
     return lines
 
 
-def frequency_file_contour_count(path, file_name, contour_size):
-    '''Outputs counted contours in a frequency file.'''
+def contour_class_file(path, file_name):
+    '''Outputs contours classes from a frequency file.'''
     complete_file = path + file_name
     frequency = freq_process(complete_file)
     contour_classes = contour_class(frequency)
+    return file_name, contour_classes
+
+
+def frequency_file_contour_count(path, file_name, contour_size):
+    '''Outputs counted contours in a frequency file.'''
+    contour_classes = contour_class_file(path, file_name)[1]
     contour_counted = contours_count(contour_classes, contour_size)
     return file_name, contour_size, contour_counted
+
+
+def percent(list):
+    '''Outputs percentuals from a list like [[(1, 0), 10],
+    [(0, 1), 11]]'''
+    sigma = sum(x[1] for x in list)
+    percent = [[n[0], "%.2f" % (float(n[1])* 100 / sigma)]
+               for n in list]
+    return percent
+
+
+def count_contours_list_of_files(path, list_of_files, n):
+    '''Count contour types in a list of frequency files in a same
+    directory.'''
+    a = [contour_subsets(contour_class_file(path, file_name)[1], n)
+         for file_name in list_of_files]
+    b = merge(a)
+    result = item_count(b)
+    return result
