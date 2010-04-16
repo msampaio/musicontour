429dfcf8e49dce1f27bdff2a7c2aa17c4519c3fd
Marcos di Silva <mdsmus@gmail.com>
2010-04-16 17:06:16 -0300

    Insert unit tests for Contour methods

--- test_contour.py
+++ test_contour.py
@@ -4,6 +4,28 @@
 import contour as c
 
 
+def test_retrograde():
+    n = c.Contour([1, 4, 9, 9, 2, 1])
+    assert n.retrograde() == [1, 2, 9, 9, 4, 1]
+
+
+def test_inversion():
+    n = c.Contour([1, 4, 9, 9, 2, 1])
+    assert n.inversion() == [9, 6, 1, 1, 8, 9]
+
+
+def test_translation():
+    n = c.Contour([1, 4, 9, 9, 2, 1])
+    assert n.translation() == [0, 2, 3, 3, 1, 0]
+
+
+def test_prime_form():
+    n1 = c.Contour([1, 4, 9, 2])
+    n2 = c.Contour([5, 7, 9, 1])
+    assert n1.prime_form() == [0, 2, 3, 1]
+    assert n2.prime_form() == [0, 3, 2, 1]
+
+
 def test_contour_class():
     assert c.contour_class([4]) == [0]
     assert c.contour_class([9, 2, 6]) == [2, 0, 1]
