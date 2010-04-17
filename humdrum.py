c55e5d1d37b316e74e147fe5fca4353c99b03eb2
Marcos di Silva <mdsmus@gmail.com>
2010-04-17 01:05:55 -0300

    Removes function based in humdrum frequency

--- humdrum.py
+++ humdrum.py
@@ -5,44 +5,6 @@ from __future__ import print_function
 import subprocess as sp
 
 
-def kern_file_process(path, basename, voice='*Ibass'):
-    """Outputs frequency values."""
-
-    cm1 = sp.Popen('extractx -i {0} {1}'.format(voice,
-                                                path + basename + ".krn"),
-                           stdout=sp.PIPE, shell=True)
-    cm2 = sp.Popen('ditto', stdin=cm1.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm3 = sp.Popen('sed \'s/^\[//g\'', stdin=cm2.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm4 = sp.Popen('sed \'s/^[0-9].*\]//g\'', stdin=cm3.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm5 = sp.Popen('sed \'s/[LJ;_]//g\'', stdin=cm4.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm6 = sp.Popen('sed \'s/^[1248]//g\'', stdin=cm5.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm7 = sp.Popen('sed \'s/^\.//g\'', stdin=cm6.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm8 = sp.Popen('freq', stdin=cm7.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm9 = sp.Popen('sed \'s/^\.//g\'', stdin=cm8.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm10 = sp.Popen('rid -GLId', stdin=cm9.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm11 = sp.Popen('egrep -v \"=|r\"', stdin=cm10.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm12 = sp.Popen('uniq', stdin=cm11.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm13 = sp.Popen('sed \'/^$/d\'', stdin=cm12.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cm14 = sp.Popen('sed \'s/X$//g\'', stdin=cm13.stdout,
-                           stdout=sp.PIPE, shell=True)
-    cmd = cm13
-    sp.Popen('mkdir -p /tmp/freq', shell=True)
-    with open("/tmp/freq/" + basename + '.freq', "w") as g:
-        print(cmd.stdout.read(), file=g)
-
-
 class Spine():
 
     def extract_spine(self):
