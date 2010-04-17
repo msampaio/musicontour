44429ffffe7fbf9ebc93190b54197c01d8f81a7f
Marcos di Silva <mdsmus@gmail.com>
2010-04-16 23:35:59 -0300

    Inserts Spine class

--- contour.py
+++ contour.py
@@ -188,6 +188,29 @@ def contour_class_one_function(path, krn_file, voice):
     return contour_class
 
 
+class Spine():
+
+    def extract_spine(self):
+        """Extracts a spine from a kern file."""
+
+        spine = sp.Popen('extractx -i {0} {1}'.format(self.v, self.f),
+                             stdout=sp.PIPE, shell=True)
+        return spine.stdout.read()
+
+    def humdrum_pitch(self):
+        """Outputs **pitch from a kern file."""
+
+        cmd1 = sp.Popen('extractx -i {0} {1}'.format(self.v, self.f),
+                        stdout=sp.PIPE, shell=True)
+        cmd2 = sp.Popen('pitch', stdin=cmd1.stdout,
+                        stdout=sp.PIPE, shell=True)
+        return cmd2.stdout.read()
+
+    def __init__(self, file, voice):
+        self.f = file
+        self.v = voice
+
+
 class Contour():
 
     def retrograde(self):
