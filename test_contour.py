1ebb1580eef1bdaf9458e0e3c733ea0ad7699f89
Marcos di Silva <mdsmus@gmail.com>
2010-04-16 23:57:56 -0300

    Cleans tests file

--- test_contour.py
+++ test_contour.py
@@ -33,33 +33,3 @@ def test_remove_adjacent():
     assert n1.remove_adjacent() == [1, 4, 9, 2, 1]
     assert n2.remove_adjacent() == [0, 1, 2, 3]
     assert n3.remove_adjacent() == [1, 4, 9, 2, 4]
-
-
-def test_contour_class():
-    assert c.contour_class([4]) == [0]
-    assert c.contour_class([9, 2, 6]) == [2, 0, 1]
-    assert c.contour_class([3, 3, 4]) == [0, 0, 1]
-    assert c.contour_class([1, 0, 1]) == [1, 0, 1]
-
-
-def test_absolute_subsets():
-    assert c.absolute_subsets([122, 424, 932, 425, 231, 229, 742],
-                             3) == [[122, 424, 932], [424, 932, 425],
-                                    [932, 425, 231], [425, 231, 229],
-                                    [231, 229, 742]]
-    assert c.absolute_subsets([122, 424, 932, 425, 231, 229, 742],
-                              7) == [[122, 424, 932, 425, 231, 229, 742]]
-
-
-def test_contour_subsets():
-    assert c.contour_subsets([122, 424, 932, 425, 231, 229, 742],
-                             3) == [[0, 1, 2], [0, 2, 1], [2, 1, 0],
-                                    [2, 1, 0], [1, 0, 2]]
-    assert c.contour_subsets([122, 424, 932, 425, 231, 229, 742],
-                             7) == [[0, 3, 6, 4, 2, 1, 5]]
-
-
-def test_contours_count():
-    assert c.contours_count([122, 424, 932, 425, 231, 229, 742],
-                            3) == [[(2, 1, 0), 2], [(0, 1, 2), 1],
-                                   [(0, 2, 1), 1], [(1, 0, 2), 1]]
