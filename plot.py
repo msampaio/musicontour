cf742ed58e6d0d9018e08d4ca562f299b6467db6
Marcos di Silva <mdsmus@gmail.com>
2010-04-19 20:05:43 -0300

    Uses a simplified code to plot preview

--- plot.py
+++ plot.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-import contour as c
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid.axislines import SubplotZero
+import pylab as pl
 
-
-def plot_cseg(cseg):
+def plot_preview(cseg):
     """Generates cseg plot.
 
     The code is based on
-    http://matplotlib.sourceforge.net/examples/axes_grid/simple_axisline.html
+    http://matplotlib.sourceforge.net/examples/pylab_examples/unicode_demo.html
     """
 
-    if 1:
-
-        fig = plt.figure(1)
-        fig.subplots_adjust(right=0.85)
-        ax = SubplotZero(fig, 1, 1, 1)
-        fig.add_subplot(ax)
-
-        ax.plot(cseg)
-        plt.draw()
-        plt.show()
+    pl.plot(cseg)
+    pl.title("Contour segment {0}".format(cseg))
+    pl.show()
