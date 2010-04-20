8f22c785768721e2a1ce8b684a9b5d92caa79b04
Marcos di Silva <mdsmus@gmail.com>
2010-04-20 15:40:31 -0300

    Inserts entry for rotation parameter

--- gui.py
+++ gui.py
@@ -41,46 +41,53 @@ class App:
                                   command=self.rotation)
         self.rotation.pack(side=tk.LEFT)
 
-        self.entry = tk.Entry(frame)
-        self.entry.pack(side=tk.LEFT)
-        self.entry.delete(0, tk.END)
-        self.entry.insert(0, "0 3 1 2")
-        self.entry.get()
+        self.cseg_entry = tk.Entry(frame)
+        self.cseg_entry.pack(side=tk.LEFT)
+        self.cseg_entry.delete(0, tk.END)
+        self.cseg_entry.insert(0, "0 3 1 2")
+        self.cseg_entry.get()
+
+        self.param_entry = tk.Entry(frame)
+        self.param_entry.pack(side=tk.LEFT)
+        self.param_entry.delete(0, tk.END)
+        self.param_entry.insert(0, "1")
+        self.param_entry.get()
 
     def plot(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(cseg)
 
     def prime_form(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(c.Contour(cseg).prime_form())
 
     def normal_form(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(c.Contour(cseg).translation())
 
     def retrograde(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(c.Contour(cseg).retrograde())
 
     def inversion(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(c.Contour(cseg).inversion())
 
     def ret_inv(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
         cseg = [int(x) for x in get.split(' ')]
         p.plot_preview(c.Contour(c.Contour(cseg).retrograde()).inversion())
 
     def rotation(self):
-        get = self.entry.get()
+        get = self.cseg_entry.get()
+        param_get = int(self.param_entry.get())
         cseg = [int(x) for x in get.split(' ')]
-        p.plot_preview(c.Contour(cseg).rotation())
+        p.plot_preview(c.Contour(cseg).rotation(param_get))
 
 
 root = tk.Tk()
