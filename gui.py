f68127fb1873519926f09a41d73ba78583a1943c
Marcos di Silva <mdsmus@gmail.com>
2010-04-20 12:16:00 -0300

    Improves GUI
    * It's a temporary gui in tkinter.

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@
 
 import Tkinter as tk
 import plot as p
+import contour as c
 
 
 class App:
@@ -15,16 +16,63 @@ class App:
         self.plot = tk.Button(frame, text="Plot", command=self.plot)
         self.plot.pack(side=tk.LEFT)
 
+        self.prime_form = tk.Button(frame, text="Prime form",
+                                    command=self.prime_form)
+        self.prime_form.pack(side=tk.LEFT)
+
+        self.normal_form = tk.Button(frame, text="Normal form",
+                                     command=self.normal_form)
+        self.normal_form.pack(side=tk.LEFT)
+
+        self.retrograde = tk.Button(frame, text="Retrograde",
+                                    command=self.retrograde)
+        self.retrograde.pack(side=tk.LEFT)
+
+        self.inversion = tk.Button(frame, text="Inversion",
+                                   command=self.inversion)
+        self.inversion.pack(side=tk.LEFT)
+
+        self.ret_inv = tk.Button(frame,
+                                              text="Ret_inv",
+                                              command=self.ret_inv)
+        self.ret_inv.pack(side=tk.LEFT)
+
         self.entry = tk.Entry(frame)
         self.entry.pack(side=tk.LEFT)
         self.entry.delete(0, tk.END)
-        self.entry.insert(0, "0 1 2")
+        self.entry.insert(0, "0 3 1 2")
         self.entry.get()
 
     def plot(self):
         get = self.entry.get()
-        out = [int(x) for x in get.split(' ')]
-        p.plot_preview(out)
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(cseg)
+
+    def prime_form(self):
+        get = self.entry.get()
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(c.Contour(cseg).prime_form())
+
+    def normal_form(self):
+        get = self.entry.get()
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(c.Contour(cseg).translate())
+
+    def retrograde(self):
+        get = self.entry.get()
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(c.Contour(cseg).retrograde())
+
+    def inversion(self):
+        get = self.entry.get()
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(c.Contour(cseg).inversion())
+
+    def ret_inv(self):
+        get = self.entry.get()
+        cseg = [int(x) for x in get.split(' ')]
+        p.plot_preview(c.Contour(c.Contour(cseg).retrograde()).inversion())
+
 
 root = tk.Tk()
 
