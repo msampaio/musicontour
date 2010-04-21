c251140523f3dac72280ce1bb53fe41c581f836a
Marcos di Silva <mdsmus@gmail.com>
2010-04-21 14:45:07 -0300

    Inserts data and test for extract_spine

--- test_humdrum.py
+++ test_humdrum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from humdrum import parse_accidentals, parse_pitch
+from humdrum import parse_accidentals, parse_pitch, Spine_file
 import re
 
 
@@ -22,3 +22,11 @@ def test_parse_pitch():
     assert parse_pitch(".") == '.'
     assert parse_pitch("*-") == '*-'
     assert parse_pitch("=") == '='
+
+
+def test_extract_spine():
+    spine = Spine_file("data/two-voices.krn", "*Ibass")
+    assert spine.extract_spine() == "!!!COM: Silva, Marcos di\n" + \
+           "!!!OTL: VLCM Test 1\n!! Test for VLCM\n**kern\n*ICvox\n" + \
+           "*Ibass\n*k[b]\n*d:\n*M3/4\n=1\n4D\n4E\n8F\n8F#\n=2\n1G\n" + \
+           ".\n.\n==\n*-\n"
