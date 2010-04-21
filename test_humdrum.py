5569bbc7f329e6963d84867281a91fd62c7de7ce
Marcos di Silva <mdsmus@gmail.com>
2010-04-21 14:44:08 -0300

    Improves module loading

--- test_humdrum.py
+++ test_humdrum.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import humdrum as h
+from humdrum import parse_accidentals, parse_pitch
 import re
 
 
 def test_parse_accidentals():
     n1 = "#"
     n2 = "b"
-    assert h.parse_accidentals(n1) == 1
-    assert h.parse_accidentals(n2) == -1
-    assert h.parse_accidentals(n1 + n1) == 2
-    assert h.parse_accidentals(n2 + n2) == -2
+    assert parse_accidentals(n1) == 1
+    assert parse_accidentals(n2) == -1
+    assert parse_accidentals(n1 + n1) == 2
+    assert parse_accidentals(n2 + n2) == -2
 
 
 def test_parse_pitch():
-    assert h.parse_pitch("**pitch") == "**midi"
-    assert h.parse_pitch("4C4") == 60
-    assert h.parse_pitch("4Eb4") == 63
-    assert h.parse_pitch("16F##4") == 67
-    assert h.parse_pitch(".") == '.'
-    assert h.parse_pitch("*-") == '*-'
-    assert h.parse_pitch("=") == '='
+    assert parse_pitch("**pitch") == "**midi"
+    assert parse_pitch("4C4") == 60
+    assert parse_pitch("4Eb4") == 63
+    assert parse_pitch("16F##4") == 67
+    assert parse_pitch(".") == '.'
+    assert parse_pitch("*-") == '*-'
+    assert parse_pitch("=") == '='
