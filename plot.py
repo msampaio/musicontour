19b818783e489521f96592104029c5f4f7a25ffb
Marcos di Silva <mdsmus@gmail.com>
2010-04-22 11:04:51 -0300

    Improves visual for multiplot function

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@
 
 from pylab import grid, axis, plot, title, legend, show, xticks, yticks, \
      figure, ylabel, xlabel
+from utils import flatten
 import random
 
 program_name = "Villa Lobos Contour Module v.0.1"
@@ -50,12 +51,25 @@ def multi_plot_preview(cseg_array):
                             [[0, 2, 3], 'green']])
     """
 
+    csegs = [cseg for cseg, color in cseg_array]
+    all_cps = flatten(csegs)
+
+    cseg_yticks = range((min(all_cps)), (max(all_cps) + 1))
+    cseg_xticks = range(max([len(x) for x in csegs]))
+
+    fig = figure() 
+    fig.canvas.set_window_title(program_name) 
+
     grid(color='b', linestyle='-', linewidth=.1)
     axis()
 
+    xlabel('c-pitch position')
+    ylabel('c-pitch')
+    xticks(cseg_xticks)
+    yticks(cseg_yticks)
     for cseg, color in cseg_array:
         plot(cseg, linewidth=2, color=color, label='{0}'.format(cseg))
 
-    title(program_name, family='georgia', size='small')
+    title(program_name + " --- Contour plotter", family='georgia', size='small')
     legend()
     show()
