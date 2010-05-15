544b54840ea9faaa145b9ae6f19b8f072aedebbb
Marcos di Silva <mdsmus@gmail.com>
2010-05-15 20:49:46 -0300

    Moves old Contour_subsets class to auxiliary functions (broken)
    * The functions are ok, but there is some problem with packages and
      modules

--- contour/auxiliary.py
+++ contour/auxiliary.py
@@ -3,6 +3,8 @@
 
 from __future__ import print_function
 from itertools import permutations
+from contour import Contour
+
 
 def permut_csegs(cardinality):
     """Returns a list of possible normalized csegs of a given
@@ -11,3 +13,38 @@ def permut_csegs(cardinality):
     base = range(cardinality)
     permutations(base, cardinality)
     return sorted(permutations(base, cardinality))
+
+
+def subsets_count(subsets_list):
+    """Counts contour subset classes with n elements."""
+
+    tuples = [tuple(x) for x in subsets_list]
+    contour_type = sorted(list(set(tuples)))
+    counted_contours = [[x, tuples.count(x)] for x in contour_type]
+    return sorted(counted_contours, key=lambda x: x[1], reverse=True)
+
+
+def normal_form_subsets(subsets_list):
+    """Outputs normal form of a list of subsets."""
+
+    return [Contour(x).translation() for x in subsets_list]
+
+
+def prime_form_subsets(subsets_list):
+    """Outputs normal form of a list of subsets."""
+
+    return [Contour(x).prime_form() for x in subsets_list]
+
+
+def normal_form_subsets_count(subsets_list):
+    """Counts subset prime forms with n elements."""
+
+    normal_form = normal_form_subsets(subsets_list)
+    return subsets_count(normal_form)
+
+
+def prime_form_subsets_count(subsets_list):
+    """Counts subset prime forms with n elements."""
+
+    prime_form = prime_form_subsets(subsets_list)
+    return subsets_count(prime_form)
