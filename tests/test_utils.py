3eba387de632707cc5434a0c46d89a3189b0bec6
Marcos di Silva <mdsmus@gmail.com>
2011-05-03 21:51:22 -0300

    Abbreviates contour.utils import

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,37 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import contour.utils
+import contour.utils as cu
 
 
 def test_double_replace():
-    assert contour.utils.double_replace("0 1 -1 1 0") == "0 + - + 0"
+    assert cu.double_replace("0 1 -1 1 0") == "0 + - + 0"
+
+
+def test_double_replace():
+    assert cu.double_replace("0 1 -1 1 0") == "0 + - + 0"
 
 
 def test_replace_list_to_plus_minus():
     l = [0, 1, 1, -1, -1]
-    assert contour.utils.replace_list_to_plus_minus(l) == "0 + + - -"
+    assert cu.replace_list_to_plus_minus(l) == "0 + + - -"
 
 
 def test_list_to_string():
-    assert contour.utils.list_to_string([1, 2, 3]) == "1 2 3"
+    assert cu.list_to_string([1, 2, 3]) == "1 2 3"
 
 
 def test_remove_adjacent_1():
     n = [1, 4, 9, 9, 2, 1]
-    assert contour.utils.remove_adjacent(n) == [1, 4, 9, 2, 1]
+    assert cu.remove_adjacent(n) == [1, 4, 9, 2, 1]
 
 
 def test_remove_adjacent_2():
     n = [0, 1, 1, 2, 3]
-    assert contour.utils.remove_adjacent(n) == [0, 1, 2, 3]
+    assert cu.remove_adjacent(n) == [0, 1, 2, 3]
 
 
 def test_remove_adjacent_3():
     n = [1, 4, 9, 9, 2, 4]
-    assert contour.utils.remove_adjacent(n) == [1, 4, 9, 2, 4]
+    assert cu.remove_adjacent(n) == [1, 4, 9, 2, 4]
 
 
 def test_remove_duplicate_tuples():
     n = [(5, 0), (4, 1), (4, 2), (9, 3), (7, 4), (9, 5), (5, 6)]
-    assert contour.utils.remove_duplicate_tuples(n) == [(5, 0), (4, 1), (9, 3),
-                                                        (7, 4), (9, 5), (5, 6)]
+    assert cu.remove_duplicate_tuples(n) == [(5, 0), (4, 1), (9, 3),
+                                             (7, 4), (9, 5), (5, 6)]
