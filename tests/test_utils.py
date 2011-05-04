58e50155e034e6c9e5be3808aeb00a53669cfc37
Marcos di Silva <mdsmus@gmail.com>
2011-05-03 22:27:14 -0300

    Inserts functional tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,38 @@
 import contour.utils as cu
 
 
-def test_double_replace():
-    assert cu.double_replace("0 1 -1 1 0") == "0 + - + 0"
+def test_flatten():
+    assert cu.flatten([[0, 1], [2, 3]]) == [0, 1, 2, 3]
+
+
+def test_filter_int_1():
+    assert cu.filter_int(2) == 2
+
+
+def test_filter_int_2():
+    assert cu.filter_int('a') == ''
+
+
+def test_filter_int_3():
+    assert cu.filter_int(set()) == ''
+
+
+def test_filter_int_4():
+    assert cu.filter_int({}) == ''
+
+
+def test_filter_int_5():
+    assert cu.filter_int([1, 2]) == ''
+
+
+def test_percent():
+    n = [[(1, 0), 10], [(0, 1), 11]]
+    assert cu.percent(n) == [[(1, 0), '47.62'], [(0, 1), '52.38']]
+
+
+def test_item_count():
+    n = [[0, 1], [2, 3], [4, 5]]
+    assert cu.item_count(n) == [[(0, 1), 1], [(4, 5), 1], [(2, 3), 1]]
 
 
 def test_double_replace():
@@ -12,8 +42,13 @@ def test_double_replace():
 
 
 def test_replace_list_to_plus_minus():
-    l = [0, 1, 1, -1, -1]
-    assert cu.replace_list_to_plus_minus(l) == "0 + + - -"
+    n = [0, 1, 1, -1, -1]
+    assert cu.replace_list_to_plus_minus(n) == "0 + + - -"
+
+
+def test_replace_plus_minus_to_list():
+    n = "0 + + - -"
+    assert cu.replace_plus_minus_to_list(n) == [0, 1, 1, -1, -1]
 
 
 def test_list_to_string():
@@ -39,3 +74,13 @@ def test_remove_duplicate_tuples():
     n = [(5, 0), (4, 1), (4, 2), (9, 3), (7, 4), (9, 5), (5, 6)]
     assert cu.remove_duplicate_tuples(n) == [(5, 0), (4, 1), (9, 3),
                                              (7, 4), (9, 5), (5, 6)]
+
+
+def test_pretty_as_cseg():
+    n = [1, 3, 5, 4]
+    assert cu.pretty_as_cseg(n) == '< 1 3 5 4 >'
+
+
+def test_greatest_first():
+    n1, n2 = [0, 1], [3, 2, 1]
+    assert cu.greatest_first(n1, n2) == [[3, 2, 1], [0, 1]]
