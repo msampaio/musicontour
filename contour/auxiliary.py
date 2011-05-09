969e3e336882775def78bfb6a443421bd29de431
Marcos di Silva <mdsmus@gmail.com>
2011-05-09 10:31:50 -0300

    Improves docstrings

--- contour/auxiliary.py
+++ contour/auxiliary.py
@@ -92,10 +92,14 @@ def interval(els):
     element in a CC (normal_form cseg here), and a later element
     as signified by +, - and a number (without + here). For
     example, in cseg = [0, 2, 1], CI(0, 2) = 2, e CI(2, 1) = -1.
+
+    >>> interval([4, 0])
+    -4
     """
 
     el1, el2 = els
     return el2 - el1
+
 
 def comparison(els):
     """Returns Morris (1987) comparison [COM(a, b)] for two
@@ -103,6 +107,9 @@ def comparison(els):
 
     This function calls interval(), but in contour theory there is no
     relation between them. This calling reason is only to reduce code.
+
+    >>> comparison([4, 0])
+    -1
     """
 
     delta = interval(els)
