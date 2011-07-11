5788a800af6c6ee43ffc9616683f6ca7de83d086
Marcos di Silva <mdsmus@gmail.com>
2011-07-11 19:40:59 -0300

    Inserts new functions and preliminary css

--- web/webcontour/views.py
+++ web/webcontour/views.py
@@ -27,20 +27,33 @@ def home(request):
 def contour(request):
     cont = request.session['contour']
     cseg = cc.Contour([int(x) for x in cont.strip().split()])
+    round_ind = 2
+
     prime_s = cseg.prime_form_sampaio()
     prime_ml = cseg.prime_form_marvin_laprade()
     retrograde = cseg.retrograde()
     inversion = cseg.inversion()
     normal = cseg.translation()
     int_1 = cseg.internal_diagonals()
+    morris_reduction = cseg.reduction_morris()
+    casv = cseg.adjacency_series_vector()
+    cia = cseg.interval_array()
+    class_index_i = round(cseg.class_index_i(), round_ind)
+    class_index_ii = round(cseg.class_index_ii(), round_ind)
+    symmetry_index = round(cseg.symmetry_index(), round_ind)
+
     cp.contour_lines_save_django([cseg, 'k', 'Original'],
                                  [prime_ml, 'r', 'Prime form ML'],
                                  [prime_s, 'b', 'Prime form S'],
                                  [retrograde, 'g', 'Retrograde'],
                                  [inversion, 'y', 'Inversion'])
+
     args = {'cseg': cseg, 'prime_s': prime_s, 'prime_ml': prime_ml,
             'retrograde': retrograde, 'inversion': inversion,
-            'normal': normal, 'int_1': int_1}
+            'normal': normal, 'int_1': int_1, 'reduction': morris_reduction,
+            'casv': casv, 'class_index_i': class_index_i,
+            'class_index_ii': class_index_ii, 'symmetry_index': symmetry_index}
+
     return render(request, 'contour.html', args)
 
 
