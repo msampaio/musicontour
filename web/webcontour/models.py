da1720ec0c00bac493bf1eefa25281aa806a1619
Marcos di Silva <mdsmus@gmail.com>
2011-07-11 10:30:26 -0300

    Improves choices

--- web/webcontour/models.py
+++ web/webcontour/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-OP_CHOICES = (('retrograde', 'Retrograde'), ('inversion', 'Inversion'),
-              ('prime_form_sampaio', 'Prime form Sampaio'),
-              ('prime_form_marvin_laprade', 'Prime form ML'),
-              ('translate', 'Normal form'), ('all', 'All operations'))
 
 class Contour(models.Model):
-    cps = models.CharField(max_length=100)
-    operation = models.CharField(max_length=30, choices=OP_CHOICES)
+
+    OP_CHOICES = (('all', 'All operations'), ('translation', 'Normal form'),
+                  ('prime_form_sampaio', 'Prime form Sampaio'),
+                  ('prime_form_marvin_laprade', 'Prime form ML'),
+                  ('retrograde', 'Retrograde'),
+                  ('inversion', 'Inversion'))
+
+    cps = models.CharField(max_length=100, default='0 2 1 3 4 5')
+    operation = models.CharField(max_length=30, choices=OP_CHOICES, default='all')
 
