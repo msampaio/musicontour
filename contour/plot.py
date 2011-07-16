ccee18bb356a35990cf1950d178cb0d2f7544aac
Marcos di Silva <mdsmus@gmail.com>
2011-07-16 14:04:07 -0300

    Changes path to run in visimus

--- contour/plot.py
+++ contour/plot.py
@@ -115,7 +115,7 @@ def contour_lines_save_django(*csegs):
     for [cseg, plot_color, legend] in csegs:
         __contour_lines(cseg, plot_color, legend)
 
-    pyplot.savefig('media/cseg_output.png', dpi=58)
+    pyplot.savefig('static/tmp/cseg_output.png', dpi=58)
 
 
 def pie(data, plot_title=""):
