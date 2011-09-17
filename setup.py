eee92d3dfe6de3babc3c9b5e3cc42a51de6e1cc0
Marcos da Silva Sampaio <marcos@sampaio.me>
2011-09-17 18:57:39 -0300

    Add more information about MusiContour

--- setup.py
+++ setup.py
@@ -13,8 +13,16 @@ setup(
     license='COPYING',
     description='This app calculates and plot musical contour operations.',
     long_description=open('README').read(),
+    platforms='Linux',
     install_requires=[
+        "python >= 2.7.1",
         "matplotlib >= 1.0.1",
-        "music21 >= 0.3.7.a11",
+        "music21 >= 0.3.7.a11"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
     ],
 )
