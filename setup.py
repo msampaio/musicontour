8cefe50824b69b1947f777d6bc73179157efc017
Marcos da Silva Sampaio <marcos@sampaio.me>
2011-09-18 15:37:52 -0300

    Simplifies distribution to visimus

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='MusiContour',
-    version='0.3.1',
+    version='visimus-0.3.1',
     author='Marcos da Silva Sampaio',
     author_email='marcos@sampaio.me',
     packages=['contour'],
@@ -15,12 +15,9 @@ setup(
     long_description=open('README').read(),
     platforms='Linux',
     install_requires=[
-        "matplotlib >= 1.0.1",
-        "music21 >= 0.3.7.a11"
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: End Users/Desktop',
         'Operating System :: POSIX',
         'Programming Language :: Python'
     ],
