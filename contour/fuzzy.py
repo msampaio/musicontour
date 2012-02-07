989548e73428e6c9900b1388d8e6bf372f5ee42e
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-02-07 19:50:06 -0200

    Insert methods, functions and tests for membership similarity

--- contour/fuzzy.py
+++ contour/fuzzy.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import contour
+import utils
+import auxiliary
 
 def membership(els):
     """Returns Fuzzy membership value. 1 if (cps1, cps2) is element of
@@ -38,17 +40,52 @@ class FuzzyMatrix(list):
     """
 
     def diagonal(self, n=1):
+        """Returns the matrix n diagonal.
+
+        >>> FuzzyMatrix([[0, 1, 1], [-1, 0, -1], [-1, 1, 0]]).diagonal()
+        [1, -1]
+        """
 
         if n < len(self):
             diagonal_size = len(self) - n
             return [self[x][x + n] for x in range(diagonal_size)]
 
     def superior_triangle(self, n=1):
+        """Returns the matrix superior triangle from a given n
+        diagonal.
+
+        >>> FuzzyMatrix([[0, 1, 1], [-1, 0, -1], [-1, 1, 0]]).superior_triangle()
+        [[1, 1], [-1]]
+        """
 
         if n < len(self):
             return [line[i + n:] for i, line in enumerate(self) if line][:-n]
+
+    def except_zero_diagonal(self):
+        """Returns the matrix without main zero diagonal.
+
+        >>> FuzzyMatrix([[0, 1, 1, 1],
+                         [0, 0, 1, 1],
+                         [0, 0, 0, 0],
+                         [0, 0, 1, 0]]).except_zero_diagonal()
+        [[1, 1, 1], [0, 1, 1], [0, 0, 0], [0, 0, 1]]
+        """
+
+        return [[el for r, el in enumerate(line) if l != r] for l, line in enumerate(self)]
 
     def __repr__(self):
 
         return "\n".join([" ".join([str(row) for row in line]) for line in self])
 
+
+def membership_similarity(cseg1, cseg2):
+    """Returns similarity from a fuzzy membership matrix. Quinn 1997.
+
+    >>> fuzzy.membership_similarity(Contour([4, 0, 1, 3, 2]), Contour([4, 1, 2, 3, 0]))
+    0.8
+    """
+
+    fuzzy1 = utils.flatten(cseg1.fuzzy_membership_matrix().except_zero_diagonal())
+    fuzzy2 = utils.flatten(cseg2.fuzzy_membership_matrix().except_zero_diagonal())
+
+    return auxiliary.position_comparison(fuzzy1, fuzzy2)
--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -20,3 +20,20 @@ def test__comparison_2():
 
 def test__comparison_3():
     assert fuzzy.comparison([3, 3]) == 0
+
+def test__FuzzyMatrix_diagonal():
+    assert fuzzy.FuzzyMatrix([[0, 1, 1], [-1, 0, -1], [-1, 1, 0]]).diagonal() == [1, -1]
+
+def test__FuzzyMatrix_superior_triangle():
+    f = [[0, 1, 1], [-1, 0, -1], [-1, 1, 0]]
+    assert fuzzy.FuzzyMatrix(f).superior_triangle() == [[1, 1], [-1]]
+
+def test__FuzzyMatrix_except_zero_diagonal():
+    f = [[0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 0], [0, 0, 1, 0]]
+    assert fuzzy.FuzzyMatrix(f).except_zero_diagonal() == [[1, 1, 1], [0, 1, 1],
+                                                           [0, 0, 0], [0, 0, 1]]
+
+def test__membership_similarity_1():
+    c1 = Contour([4, 0, 1, 3, 2])
+    c2 = Contour([4, 1, 2, 3, 0])
+    assert fuzzy.membership_similarity(c1, c2) == 0.8
