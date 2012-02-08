00f89525f3a81b69dfb732ad0d87543bea239911
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-02-08 20:21:49 -0200

    Inserts functions and tests for fuzzy extension
    * average comparison matrix from average matrix
    * average comparison matrix from csegs

--- contour/fuzzy.py
+++ contour/fuzzy.py
@@ -5,6 +5,7 @@ import contour
 import utils
 import auxiliary
 import numpy
+import itertools
 
 
 def membership(els):
@@ -95,7 +96,7 @@ def membership_similarity(cseg1, cseg2):
 
 def average_matrix(*csegs):
     """Returns the matrix of an average contour from a list of
-    contours.
+    contours. Quinn 1997.
 
     >>> average_matrix(Contour([3, 0, 1, 2, 1]), Contour([4, 0, 1, 3, 2]), Contour([4, 1, 2, 3, 0]))
     [[0.0, 0.0, 0.0, 0.0, 0.0],
@@ -109,3 +110,45 @@ def average_matrix(*csegs):
     return [list(sum(a) / float(len(matrices))) for a in zip(*matrices)]
 
 
+def average_comparison(average_matrix):
+    """Returns a comparison matrix from an average matrix.
+
+    >>> average_comparison([[0.0, 0.0, 0.0, 0.0, 0.0],
+                            [1.0, 0.0, 1.0, 1.0, 0.66666666666666663],
+                            [1.0, 0.0, 0.0, 1.0, 0.33333333333333331],
+                            [1.0, 0.0, 0.0, 0.0, 0.0],
+                            [1.0, 0.33333333333333331, 0.33333333333333331, 1.0, 0.0]])
+    [[0.0, -1.0, -1.0, -1.0, -1.0],
+    [1.0, 0.0, 1.0, 1.0, 0.3333333333333333],
+    [1.0, -1.0, 0.0, 1.0, 0.0],
+    [1.0, -1.0, -1.0, 0.0, -1.0],
+    [1.0, -0.3333333333333333, 0.0, 1.0, 0.0]]
+    """
+
+    def __comparison(matrix, a, b):
+        return matrix.item((x, y)) - matrix.item((y, x))
+
+    def __product(rsize, n):
+        return itertools.product(range(n, n + 1), rsize)
+
+    size = len(average_matrix)
+    rsize = range(size)
+    matrix = numpy.matrix(average_matrix)
+
+    return [[__comparison(matrix, x, y)  for x, y in __product(rsize, n)] for n in rsize]
+
+
+def average_comparison_from_csegs(*csegs):
+    """Returns a comparison matrix from a list of contours.
+
+    >>> average_comparison_from_csegs(Contour([3, 0, 1, 2, 1]),
+                                      Contour([4, 0, 1, 3, 2]),
+                                      Contour([4, 1, 2, 3, 0]))
+    [[0.0, -1.0, -1.0, -1.0, -1.0],
+    [1.0, 0.0, 1.0, 1.0, 0.3333333333333333],
+    [1.0, -1.0, 0.0, 1.0, 0.0],
+    [1.0, -1.0, -1.0, 0.0, -1.0],
+    [1.0, -0.3333333333333333, 0.0, 1.0, 0.0]]
+    """
+
+    return average_comparison(average_matrix(*csegs))
