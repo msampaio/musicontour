88e01122d5b8c347035456e8a5aefa155027c3f1
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-02-08 14:29:09 -0200

    Inserts comparison matrix display method

--- contour/matrix.py
+++ contour/matrix.py
@@ -48,6 +48,22 @@ class ComparisonMatrix(list):
         if n < len(self):
             return [line[i + n:] for i, line in enumerate(self) if line][:-n]
 
+    def display(self):
+        """Returns matrix with a matrix with cseg in a visual way.
+
+        >>> ComparisonMatrix([[0, 1, 1, 1], [-1, 0, -1, 1], [-1, 1, 0, 1], [-1, -1, -1, 0]]).display()
+        '  | 0 2 1 3\n-----------\n0 | 0 + + +\n2 | - 0 - +\n1 | - + 0 +\n3 | - - - 0'
+        """
+
+        def __lines(el, line, cseg):
+            return str(cseg[el]) + " | " + str(utils.replace_list_to_plus_minus(line))
+
+        cseg = self.cseg()
+        first_line = "  | {0}\n".format(" ".join([str(x) for x in cseg]))
+        second_line = "---" + ("-" * len(self) * 2) + "\n"
+        other_lines = "\n".join([__lines(el, line, cseg) for el, line in enumerate(self)])
+        return first_line + second_line + other_lines
+
     def __repr__(self):
 
         return "\n".join([str(utils.replace_list_to_plus_minus(line)) for line in self])
