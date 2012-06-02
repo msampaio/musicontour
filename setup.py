bbc321419d0f4b6b418d7869f0a9962c145ed4ad
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-06-02 00:44:43 -0300

    Insert initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MusiContour',
+      description='Musical contour relations calculator',
+      author='Marcos da Silva Sampaio',
+      author_email='marcos@sampaio.me',
+      packages=['contour', 'web', 'tests'],
+     )
